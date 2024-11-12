d378961edd8032214e009f1cde40e1c075ff3719
ZephyroCode <royceskycraft@gmail.com>
2024-11-12 16:04:31 -0400

    Add normalize.css and starting rework

--- renderer.js
+++ renderer.js
@@ -1,30 +1,30 @@
-const rootStyles = document.documentElement.style
-const redInput = document.getElementById('red-input')
-const greenInput = document.getElementById('green-input')
-const blueInput = document.getElementById('blue-input')
-const button = document.getElementById('button')
-const resultText = document.getElementById('result-text')
-let redValue
-let greenValue
-let blueValue
+// const rootStyles = document.documentElement.style
+// const redInput = document.getElementById('red-input')
+// const greenInput = document.getElementById('green-input')
+// const blueInput = document.getElementById('blue-input')
+// const button = document.getElementById('button')
+// const resultText = document.getElementById('result-text')
+// let redValue
+// let greenValue
+// let blueValue
 
-redInput.addEventListener('change', (e) => {
-  let value = e.target.value
-  redValue = value
-})
+// redInput.addEventListener('change', (e) => {
+//   let value = e.target.value
+//   redValue = value
+// })
 
-greenInput.addEventListener('change', (e) => {
-  let value = e.target.value
-  greenValue = value
-})
+// greenInput.addEventListener('change', (e) => {
+//   let value = e.target.value
+//   greenValue = value
+// })
 
-blueInput.addEventListener('change', (e) => {
-  let value = e.target.value
-  blueValue = value
-})
+// blueInput.addEventListener('change', (e) => {
+//   let value = e.target.value
+//   blueValue = value
+// })
 
-button.addEventListener('click', (e) => {
-  let resultValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`
-  rootStyles.setProperty('--result', resultValue)
-  resultText.innerText = `R: ${redValue} G: ${greenValue} B: ${blueValue}`
-})
+// button.addEventListener('click', (e) => {
+//   let resultValue = `rgb(${redValue}, ${greenValue}, ${blueValue})`
+//   rootStyles.setProperty('--result', resultValue)
+//   resultText.innerText = `R: ${redValue} G: ${greenValue} B: ${blueValue}`
+// })
