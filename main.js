46241e1775c460c4d28ee454e7cf8c477f13d924
ZephyroCode <royceskycraft@gmail.com>
2024-11-12 15:22:06 -0400

    Update files formatting

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
   })
 
   win.loadFile('index.html')
